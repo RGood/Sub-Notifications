5c581b2dbf162c15f2b247026f3df0486151efad
root <root@justintbassett.com>
2017-03-01 13:17:59 -0800

    Comply with praw4 message structure convention and error logging.

--- oauth_notifications.py
+++ oauth_notifications.py
@@ -156,7 +156,7 @@ def handle_mail():
 						m.reply('Unable to parse subreddit. Please double-check the subreddit(s) being unsubscribed from.')
 						return
 
-					target = ("/r/" + m.subreddit.lower()) if (m.subreddit != None) else (m.author)
+					target = ("/r/" + m.subreddit.lower()) if (m.subreddit != None) else (m.author.name)
 					print("Unsubscribing " + target + " from " + body['subreddit'])
 					subreddits = body['subreddit'].split(' ')
 					for s in subreddits:
@@ -166,6 +166,7 @@ def handle_mail():
 						names+=s+"    \n"
 					m.reply("You have been successfully unsubscribed from the following subreddit" + (':\n\n' if len(subreddits)==1 else 's:\n\n') + names)
 				except:
+					traceback.print_exc(file=sys.stdout)
 					print("Error parsing unsubscribe request.")
 					m.reply("There was an error processing your request. Please check the JSON syntax and try again.\n\nIf you cannot resolve the problem, please message /u/The1RGood.")
 			elif m.subject == "Action: Subscribe" and m.parent_id == None:
@@ -190,7 +191,7 @@ def handle_mail():
 
 					print("Filters made")
 
-					target = ("/r/" + m.subreddit.lower()) if (m.subreddit != None) else (m.author)
+					target = ("/r/" + m.subreddit.lower()) if (m.subreddit != None) else (m.author.name)
 					print("Subscribing " + target + " to " + body['subreddit'])
 					subreddits = body['subreddit'].split(' ')
 					for s in subreddits:
@@ -200,6 +201,7 @@ def handle_mail():
 						names+=s+"    \n"
 					m.reply("You have been successfully subscribed to the following subreddit" + (':\n\n' if len(subreddits)==1 else 's:\n\n') + names)
 				except:
+					traceback.print_exc(file=sys.stdout)
 					print("Error parsing subscribe request.")
 					m.reply("There was an error processing your request. Please check the JSON syntax and try again.\n\nIf you cannot resolve the problem, please message /u/The1RGood.")
 
